ff0b9226bdb4fdb7aef43745d921edf385d0face
daramacmeanmain <dara.macmeanmain@outlook.ie>
2017-12-11 01:44:20 +0000

    basic sorta functional thing

---
+++ mobAppsProject2017/Models/MovieBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Data;
+
+
+
+namespace Models
+
+{
+
+    public class MovieBase
+
+    {
+
+        public List<Movie> Movies { get; set; }
+
+        public String Name { get; set; }
+
+
+
+        public MovieBase(String databaseName)
+
+        {
+
+            Name = databaseName;
+
+            Movies = FakeService.GetMovies();
+
+        }
+
+
+
+        public void Add(Movie movie)
+        {
+
+            if (!Movies.Contains(movie))
+
+            {
+
+                Movies.Add(movie);
+
+                FakeService.Write(movie);
+
+            }
+
+        }
+
+
+
+        public void Delete(Movie movie)
+
+        {
+
+            if (Movies.Contains(movie))
+
+            {
+
+                Movies.Remove(movie);
+
+                FakeService.Delete(movie);
+
+            }
+
+        }
+
+
+
+        public void Update(Movie movie)
+
+        {
+
+            FakeService.Write(movie);
+
+        }
+
+    }
+
+}
